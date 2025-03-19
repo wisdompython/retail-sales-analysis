977b5000e590c81544021fcb9897cf17cf990bb9
Wisdom <idriswisdom6@gmail.com>
2025-03-19 14:06:01 +0100

    configure state backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,6 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock-table"
+    use_lockfile = true
   }
 }
