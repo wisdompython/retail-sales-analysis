cfd1e002a60fe82f554f0f43a4b489a91266e6c7
Wisdom <idriswisdom6@gmail.com>
2025-04-04 10:46:10 +0100

    add kaggle setup

--- tf-resources/variables.tf
+++ tf-resources/variables.tf
@@ -1,11 +1,13 @@
 variable "kaggle_username" {
   type      = string
   sensitive = true # Mark as sensitive to avoid logging
+  default = "wisdomabiolaidris"
 }
 
 variable "kaggle_key" {
   type      = string
   sensitive = true # Mark as sensitive to avoid logging
+  default = "10185880d60f1d2757e94a7752a29155"
 }
 
 variable "vpc_id" {
